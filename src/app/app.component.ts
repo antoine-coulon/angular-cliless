6377058814601020e068c8b6e4c9c59c4294d87f
coulon_antoine <coulon_antoine@outlook.fr>
2020-10-22 23:49:20 +0200

    Initial bootstrapping.

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  template: "<h1>Hello world!</h1>",
+})
+export class AppComponent {
+  constructor() {
+    console.log("I am Angular!");
+  }
+}
